2ee16846514d3494d9763a836ed274c239cb0827
RexndleZero <raihanaditya1506@gmail.com>
2024-07-20 00:32:03 +0700

    update #3

--- my-profile/app/components/Herosection.jsx
+++ my-profile/app/components/Herosection.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Herosection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center">
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
             Hello my name is reyy!
