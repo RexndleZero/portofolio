870fe03de2a0ddc55ab4ad47822e4b5d33ed2190
RexndleZero <raihanaditya1506@gmail.com>
2024-07-20 00:17:14 +0700

    update #2

--- my-profile/app/components/Herosection.jsx
+++ my-profile/app/components/Herosection.jsx
@@ -9,13 +9,22 @@ const Herosection = () => {
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
             Hello my name is reyy!
           </h1>
-          <p className="text-[#ADB7BE] text-lg lf:text-xl">lorem ipsum</p>
+          <p className="text-[#ADB7BE] text-lg mb-6 lf:text-xl">lorem ipsum</p>
+          <div>
+            <button className="px-6 py-3 rounded-full mr-4 bg-[#009] hover:bg-slate-950 text-black">
+              Hire me!
+            </button>
+            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-900 text-black border border-[#150d5c] mt-3 ">
+              Download CV Here!
+            </button>
+          </div>
         </div>
-        <div className="col-span-5">
-          <div className="rounded-full bg-[#181818]">
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400] lg:h-[400] relative">
             <Image
               src="/images/heros-image.jpg"
               alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
             />
--- my-profile/app/page.js
+++ my-profile/app/page.js
@@ -4,7 +4,9 @@ import Herosection from "./components/Herosection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#1b1b1b] container mx-auto px-12 py-4">
-      <Herosection />
+      <div class="container mx-auto px-12 py-4 ">
+        <Herosection />
+      </div>
     </main>
   );
 }
